3cd36e138f321d76a4305d319b7b2c2683978c31
Martin Kirchgessner <martin.kirch@gmail.com>
2020-05-09 19:08:43 +0200

    adding force_genre_and_language_by_folder

---
+++ examples/force_genre_and_language_by_folder.py
@@ -0,0 +1,49 @@
+def new_item(item, path):
+    """
+    This is a variant of `force_genre_by_folder.py` where we may also force
+    the `language` using a sub-subfolder.
+    It assumes that the dropbox contains folders like:
+
+    /home/me/my-beets-dropbox/
+    ├── Rock
+         └── fra
+    ├── Rap
+         └── fra
+         └── eng
+    ├── Pop
+         └── fra
+    ├── Instrumental
+    └── Classical
+
+    The first level of folders will force the genre.
+    For example, sropping a file in `Instrumental` would import it with `genre:Instrumental`.
+    The tag value will match the folder name, and you could create as many as you want.
+    The sub-level is optional and may be used to force the `language` tag;
+    again the value we put in the tag will be the folder's name
+    (we test that this name is only 3 characters, just in case someone drops accidently a folder).
+    So dropping a file in `Rap/fra` would import it with `genre:Rap` and
+    `language:fra`.
+
+    Dropping a file in the root folder `my-beets-dropbox` would do nothing
+    because of the test on the 3 first lines. In that case we could also just
+    `return item` to still import it and leave tags untouched.
+    """
+    if not path:
+        logging.info("No sub-folder, leaving the file for manual import")
+        return None
+
+    # remove first /
+    path = path[1:]
+    path_parts = path.split('/')
+    custom_tags = {}
+
+    if len(path_parts) >= 1:
+        custom_tags['genre'] = path_parts[0]
+
+    if len(path_parts) >= 2 and len(path_parts[1]) == 3:
+        custom_tags['language'] = path_parts[1]
+
+    if custom_tags:
+        logging.info("Applying %s", custom_tags)
+        item.update(custom_tags)
+    return item
