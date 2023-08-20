855a3fc28c8643c6acacac90526ff6bf2dda9c53
Martin Kirchgessner <martin.kirch@gmail.com>
2023-08-20 18:13:56 +0200

    implement a more general debouncing

--- beetsplug/drop2beets.py
+++ beetsplug/drop2beets.py
@@ -4,15 +4,13 @@ import os
 import subprocess
 from os.path import expanduser
 from time import time
-from collections import OrderedDict
 
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, FileSystemEvent, FileClosedEvent, FileMovedEvent
+from watchdog.events import FileSystemEventHandler, FileSystemEvent, FileMovedEvent
 
 from beets import config
 from beets.plugins import BeetsPlugin
-from beets.ui import Subcommand
-from beets.ui.commands import import_files
+from beets.ui import Subcommand, commands
 
 
 _logger = logging.getLogger("drop2beets")
@@ -31,51 +29,59 @@ WantedBy=default.target
 """
 
 class Drop2BeetsHandler(FileSystemEventHandler):
-    # We must debounce the FileClosedEvent because it fires again right after
-    # `import_files`, maybe because beets writes tags before moving the file.
-    # It's compared to time() deltas to it's measured in seconds; it's set
-    # to the time that might take a looooong single import.
+    """
+    This class handles events in drop2beets' target folder.
+
+    Implementation note:
+    We must debounce events and import only a few seconds after the last one
+    because no Watchdog event matches our definition of "a file appears". For
+    example:
+     - moving a file to the watched folder fires only FileCreatedEvent
+     - moving a file *in* the watched folder fires FileMovedEvent
+     - (s)cp a file to the watched folder fires FileCreatedEvent/FileModifiedEvent/FileClosedEvent
+    also, starting the importation might fire an event on the file too.
+    """
+
+    # How many seconds should we wait for events to stop before importing
     DEBOUNCE_WINDOW = 10
 
     def __init__(self, lib):
         self.lib = lib
-        self.debounce = OrderedDict()
+        self.debounce = {}
         super().__init__()
 
-    def forget_debounce(self):
+    def try_to_import(self):
         """
-        remove paths from the debounce window
+        Import paths that had no event for a few seconds (following DEBOUNCE_WINDOW).
+        Cleanup paths that have been imported.
         """
         if self.debounce:
             limit = time() - self.DEBOUNCE_WINDOW
             for path, timestamp in list(self.debounce.items()):
-                if timestamp <= limit:
+                if timestamp <= 0:
                     del self.debounce[path]
-                else:
-                    break # exploit the fact that dict entries are sorted by insertion order
+                elif timestamp <= limit:
+                    self.debounce[path] = -1
+                    _logger.info("Processing %s", path)
+                    commands.import_files(self.lib, [path], None)
 
     def on_any_event(self, event:FileSystemEvent):
         _logger.debug("got %r", event)
-        self.forget_debounce()
         if event and not event.is_directory:
             if isinstance(event, FileMovedEvent):
                 fullpath = event.dest_path
-            elif isinstance(event, FileClosedEvent):
-                fullpath = event.src_path
-                if fullpath in self.debounce:
-                    return
-                else:
-                    self.debounce[fullpath] = time()
             else:
-                return
-            _logger.info("Processing %s", fullpath)
-            import_files(self.lib, [fullpath], None)
+                fullpath = event.src_path
+            current = self.debounce.get(fullpath, 1)
+            if current > 0:
+                self.debounce[fullpath] = time()
 
 
 class Drop2BeetsPlugin(BeetsPlugin):
 
     def __init__(self):
         super(Drop2BeetsPlugin, self).__init__()
+        self.observer = None
         self.attributes = None
         self.dropbox_path = self.config['dropbox_path'].as_filename()
 
@@ -141,7 +147,13 @@ class Drop2BeetsPlugin(BeetsPlugin):
         self.observer.schedule(handler, self.dropbox_path, recursive=True)
         _logger.info("Drop2beets starting to watch %s", self.dropbox_path)
         self.observer.start()
-        self.observer.join()
+        try:
+            while self.observer.isAlive():
+                self.observer.join(1)
+                handler.try_to_import()
+        finally:
+            self.observer.stop()
+            self.observer.join()
 
     def _install(self, lib, opts, args):
         logging.basicConfig(
@@ -149,7 +161,7 @@ class Drop2BeetsPlugin(BeetsPlugin):
             format="%(asctime)s [%(filename)s:%(lineno)s] %(levelname)s %(message)s"
         )
         beet_path = subprocess.getoutput("which beet")
-        print("beet found in %s" % beet_path)
+        print(f"beet found in {beet_path}")
         with open("drop2beets.service", "w") as service_file:
             service_file.write(_SERVICE_TEMPLATE.format(beet_path=beet_path))
 
--- tests/test_drop2beets.py
+++ tests/test_drop2beets.py
@@ -1,16 +1,50 @@
-from time import time, sleep
+from time import sleep
+from watchdog.events import FileSystemEvent, FileMovedEvent
+from beets.ui import commands
 from beetsplug.drop2beets import Drop2BeetsHandler
 
-def test_debounce():
+def test_debounce(monkeypatch):
     handler = Drop2BeetsHandler(None)
-    handler.forget_debounce()
-    assert len(handler.debounce) == 0
 
-    handler.debounce["some/song.flac"] = 1692542424 # time of writing this test
-    handler.debounce["new_song.flac"] = time()
-    handler.forget_debounce()
-    assert len(handler.debounce) == 1
+    imported_paths = None
+    def fake_import (lib, paths, query):
+        nonlocal imported_paths
+        imported_paths = paths
+        # the following flag shows that import has started: future events should be ignored
+        # also, we expect exactly one path here
+        assert handler.debounce[imported_paths[0]] < 0
+    monkeypatch.setattr(commands, "import_files", fake_import)
+
+    imported_paths = None
+    handler.try_to_import()
+    assert imported_paths is None # nothing to import
+
+    handler.on_any_event(FileSystemEvent("some/songA.flac"))
+    imported_paths = None
+    handler.try_to_import()
+    assert imported_paths is None
+
+    sleep(Drop2BeetsHandler.DEBOUNCE_WINDOW / 2.)
+    handler.on_any_event(FileMovedEvent("/some/store/songB.flac", "some/songB.flac"))
+    imported_paths = None
+    handler.try_to_import()
+    assert imported_paths is None
+
+    sleep(Drop2BeetsHandler.DEBOUNCE_WINDOW / 2. + 0.01)
+    handler.on_any_event(FileSystemEvent("some/songB.flac"))
+    imported_paths = None
+    handler.try_to_import()
+    assert imported_paths == ["some/songA.flac"]
+    # new events on songA should be ignored because they're likely caused by Beets' import
+    handler.on_any_event(FileSystemEvent("some/songA.flac"))
+
+    sleep(Drop2BeetsHandler.DEBOUNCE_WINDOW / 2. + 0.01)
+    imported_paths = None
+    handler.try_to_import()
+    assert imported_paths is None
+
     sleep(Drop2BeetsHandler.DEBOUNCE_WINDOW + 0.01)
-    handler.forget_debounce()
-    assert len(handler.debounce) == 0
+    imported_paths = None
+    handler.try_to_import()
+    assert imported_paths == ["some/songB.flac"]
 
